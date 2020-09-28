c769171884a72968c961eaa1a79d21cdc9bcdf55
Hang Lou <lhang39@hotmail.com>
2020-09-28 14:40:44 +0100

    update

--- src/models/CoxPHM/eval_CoxPHM_signature.py
+++ src/models/CoxPHM/eval_CoxPHM_signature.py
@@ -5,76 +5,99 @@ from src.features.scaler import *
 from definitions import *
 from src.features.dicts import *
 from src.omni.functions import *
-def Cox_model_input_output(df, df2, a1, normalize=False):
-    # construct output as reuiqred np structrued array format
 
-    df['censor_hours'] = a1 + 1 - df.groupby('icustay_id')['SepsisLabel'].cumsum()
+
+def Coxph_df(df, features, feature_dict, T, labels):
+    """
+    :param df:
+    :param features:
+    :param feature_dict:
+    :param T:
+    :param labels:
+    :return:
+    """
+
+    df['censor_hours'] = T + 1 - df.groupby('icustay_id')['SepsisLabel'].cumsum()
+
+    # construct output as reuiqred np structrued array format
+    rest_features = [str(i) for i in range(features.shape[1])[len(feature_dict):]]
+    total_features = original_features + rest_features
+    df2 = pd.DataFrame(features, columns=total_features)
 
     df2.replace(np.inf, np.nan, inplace=True)
     df2.replace(-np.inf, np.nan, inplace=True)
     df2 = df2.fillna(-1)
 
-    if normalize:
-        standard_scaler = StandardScaler()
-        x_scaled = standard_scaler.fit_transform(df2[df2.columns[1:]].values)
-        df2[df2.columns[1:]] = x_scaled
-    df2['label'] = df['SepsisLabel'].values
+    df2['label'] = labels
     df2['censor_hours'] = df['censor_hours'].values
 
     return df2
 
-original_features = feature_dict_james['vitals'] + feature_dict_james['laboratory'] \
-                        + feature_dict_james['derived'] + ['age', 'gender', 'hour', 'HospAdmTime']
 
-coxph_hyperparameter= {
-    'regularize_sofa':0.01,
-    'step_size_sofa':0.3,
-    'regularize_suspicion':0.01,
-    'step_size_suspicion':0.3,
-    'regularize_sepsis_min':0.01,
-    'step_size_sepsis_min':0.3,
+def Coxph_eval(df, model, T, save_dir):
+    """
+    :param df:
+    :param model:
+    :param T:
+    :param save_dir:
+    :return:
+    """
+    # predict and evalute on test set
+    H_t = model.predict_cumulative_hazard(df).iloc[T + 1, :]
+    risk_score = 1 - np.exp(-H_t)
+
+    df['risk_score'] = risk_score
+    np.save(save_dir, df['risk_score'])
+    fpr, tpr, thresholds = roc_curve(df_coxph_test['label'], df_coxph_test['risk_score'], pos_label=1)
+    index = np.where(tpr >= 0.85)[0][0]
+    auc_score, specificity = auc(fpr, tpr), 1 - fpr[index]
+
+    test_pred_labels = (df_coxph_test['risk_score'] > thresholds[index]).astype('int')
+    accuracy = accuracy_score(df_coxph_test['label'].values, test_pred_labels)
+    print('auc, sepcificity,accuracy', auc_score, specificity, accuracy)
+
+    return auc_score, specificity, accuracy
+
+
+# feature dictionary for coxph model
+original_features = feature_dict_james['vitals'] + feature_dict_james['laboratory'] \
+                    + feature_dict_james['derived'] + ['age', 'gender', 'hour', 'HospAdmTime']
+
+coxph_hyperparameter = {
+    'regularize_sofa': 0.01,
+    'step_size_sofa': 0.3,
+    'regularize_suspicion': 0.01,
+    'step_size_suspicion': 0.3,
+    'regularize_sepsis_min': 0.01,
+    'step_size_sepsis_min': 0.3,
 }
 
 if __name__ == '__main__':
-    x,y=24,12
+    x, y = 24, 12
     Save_Dir = DATA_DIR + '/processed/experiments_' + str(x) + '_' + str(y) + '/'
 
-    results=[]
-    for definition in ['t_sofa','t_suspicion','t_sepsis_min']:
-        for T in [4,6,8,12]:
+    results = []
+    for definition in ['t_sofa', 't_suspicion', 't_sepsis_min']:
+        for T in [4, 6, 8, 12]:
             print('load dataframe and input features')
-            df_sepsis_test = pd.read_pickle(Save_Dir+definition[1:]+'_dataframe_test.pkl')
+            df_sepsis_test = pd.read_pickle(Save_Dir + definition[1:] + '_dataframe_test.pkl')
             features_test = np.load(Save_Dir + 'james_features_test' + definition[1:] + '.npy')
-
-
-            rest_features = [str(i) for i in range(features_test.shape[1])[len(original_features):]]
-            total_features = original_features + rest_features
-            df2_test = pd.DataFrame(features_test,
-                             columns=total_features)
-
             print('load test labels')
-            scores = np.load(Save_Dir + 'scores' + definition[1:] + '_' + str(T) + '.npy')
             labels_test = np.load(Save_Dir + 'label_test' + definition[1:] + '_' + str(T) + '.npy')
-            df_sepsis_test['SepsisLabel'] = labels_test
 
-            df_coxph_test = Cox_model_input_output(df_sepsis_test,df2_test, T, True)
+            # prepare dataframe for coxph model
+            df_coxph_test = Coxph_df(df_sepsis_test, features_test, original_features, T, labels_test)
 
-            #load trained coxph model
-            cph = load_pickle(MODELS_DIR+'CoxPHM/'+str(x)+'_'+str(y)+'_'+str(T)+definition[1:])
+            # load trained coxph model
+            cph = load_pickle(MODELS_DIR + 'CoxPHM/' + str(x) + '_' + str(y) + '_' + str(T) + definition[1:])
 
             # predict and evalute on test set
-            H_t = cph.predict_cumulative_hazard(df_coxph_test).iloc[T + 1, :]
-            risk_score = 1 - np.exp(-H_t)
-            df_coxph_test['risk_score'] = risk_score
-            np.save(OUTPUT_DIR +'predictions/'+ 'coxphm_' + definition[1:] + '_' + str(T) + '.npy', df_coxph_test['risk_score'])
-            fpr, tpr, thresholds = roc_curve(df_coxph_test['label'], df_coxph_test['risk_score'], pos_label=1)
-            index = np.where(tpr >= 0.85)[0][0]
-            auc_score, specificity = auc(fpr, tpr), 1 - fpr[index]
+            auc_score, specificity, accuracy = Coxph_eval(df_sepsis_test, cph, T,
+                                                          OUTPUT_DIR + 'predictions/' + 'coxphm_' +
+                                                          definition[1:] + '_' + str(T) + '.npy')
 
-            test_pred_labels = (df_coxph_test['risk_score'] > thresholds[index]).astype('int')
-            accuracy = accuracy_score(df_coxph_test['label'].values, test_pred_labels)
-            print('auc, sepcificity,accuracy', auc_score, specificity, accuracy)
             results.append([str(x) + ',' + str(y), T, definition, auc_score, specificity, accuracy])
-    print(results)
+
+    # save numerical results
     result_df = pd.DataFrame(results, columns=['x,y', 'T', 'definition', 'auc', 'speciticity', 'accuracy'])
-    result_df.to_csv(OUTPUT_DIR + 'numerical_results/'+'CoxPHM/' +str(x)+','+str(y)+"_results.csv")
+    result_df.to_csv(OUTPUT_DIR + 'numerical_results/' + 'CoxPHM/' + str(x) + ',' + str(y) + "_results.csv")
--- src/models/CoxPHM/train_CoxPHM_signature.py
+++ src/models/CoxPHM/train_CoxPHM_signature.py
@@ -4,62 +4,66 @@ from definitions import *
 from lifelines import CoxPHFitter
 from src.features.dicts import *
 from src.omni.functions import *
-def Cox_model_input_output(df, df2, a1, normalize=False):
-    # construct output as reuiqred np structrued array format
 
-    df['censor_hours'] = a1 + 1 - df.groupby('icustay_id')['SepsisLabel'].cumsum()
+
+def Coxph_df(df, features, feature_dict, T, labels):
+    """
+    :param df:
+    :param features:
+    :param feature_dict:
+    :param T:
+    :param labels:
+    :return:
+    """
+
+    df['censor_hours'] = T + 1 - df.groupby('icustay_id')['SepsisLabel'].cumsum()
+
+    # construct output as reuiqred np structrued array format
+    rest_features = [str(i) for i in range(features.shape[1])[len(feature_dict):]]
+    total_features = original_features + rest_features
+    df2 = pd.DataFrame(features, columns=total_features)
 
     df2.replace(np.inf, np.nan, inplace=True)
     df2.replace(-np.inf, np.nan, inplace=True)
     df2 = df2.fillna(-1)
 
-    if normalize:
-        standard_scaler = StandardScaler()
-        x_scaled = standard_scaler.fit_transform(df2[df2.columns[1:]].values)
-        df2[df2.columns[1:]] = x_scaled
-    df2['label'] = df['SepsisLabel'].values
+    df2['label'] = labels
     df2['censor_hours'] = df['censor_hours'].values
 
     return df2
 
-original_features = feature_dict_james['vitals'] + feature_dict_james['laboratory'] \
-                        + feature_dict_james['derived'] + ['age', 'gender', 'hour', 'HospAdmTime']
 
-coxph_hyperparameter= {
-    'regularize_sofa':0.01,
-    'step_size_sofa':0.3,
-    'regularize_suspicion':0.01,
-    'step_size_suspicion':0.3,
-    'regularize_sepsis_min':0.01,
-    'step_size_sepsis_min':0.3,
+original_features = feature_dict_james['vitals'] + feature_dict_james['laboratory'] \
+                    + feature_dict_james['derived'] + ['age', 'gender', 'hour', 'HospAdmTime']
+
+coxph_hyperparameter = {
+    'regularize_sofa': 0.01,
+    'step_size_sofa': 0.3,
+    'regularize_suspicion': 0.01,
+    'step_size_suspicion': 0.3,
+    'regularize_sepsis_min': 0.01,
+    'step_size_sepsis_min': 0.3,
 }
 
 if __name__ == '__main__':
-    x,y=24,12
+    x, y = 24, 12
     Save_Dir = DATA_DIR + '/processed/experiments_' + str(x) + '_' + str(y) + '/'
 
-
-    for definition in ['t_sofa','t_suspicion','t_sepsis_min']:
-        for T in [4,6,8,12]:
+    for definition in ['t_sofa', 't_suspicion', 't_sepsis_min']:
+        for T in [4, 6, 8, 12]:
             print('load dataframe and input features')
-            df_sepsis_train = pd.read_pickle(Save_Dir+definition[1:]+'_dataframe_train.pkl')
+            df_sepsis_train = pd.read_pickle(Save_Dir + definition[1:] + '_dataframe_train.pkl')
             features_train = np.load(Save_Dir + 'james_features' + definition[1:] + '.npy')
 
-
-            rest_features = [str(i) for i in range(features_train.shape[1])[len(original_features):]]
-            total_features = original_features + rest_features
-            df2_train = pd.DataFrame(features_train,
-                             columns=total_features)
-
             print('load train labels')
-            scores = np.load(Save_Dir + 'scores' + definition[1:] + '_' + str(T) + '.npy')
             labels_train = np.load(Save_Dir + 'label' + definition[1:] + '_' + str(T) + '.npy')
-            df_sepsis_train['SepsisLabel'] = labels_train
 
-            df_coxph_train = Cox_model_input_output(df_sepsis_train, df2_train, T, True)
-            cph = CoxPHFitter(penalizer=coxph_hyperparameter['regularize'+definition[1:]])\
-               .fit(df_coxph_train, duration_col='censor_hours', event_col='label',
-                    show_progress=True, step_size=coxph_hyperparameter['step_size'+definition[1:]])
-            save_pickle(cph,MODELS_DIR+'CoxPHM/'+str(x)+'_'+str(y)+'_'+str(T)+definition[1:])
+            # prepare dataframe for coxph model
+            df_coxph_train = Coxph_df(df_sepsis_train, features_train, original_features, T, labels_train)
 
+            # fit CoxPHM
+            cph = CoxPHFitter(penalizer=coxph_hyperparameter['regularize' + definition[1:]]) \
+                .fit(df_coxph_train, duration_col='censor_hours', event_col='label',
+                     show_progress=True, step_size=coxph_hyperparameter['step_size' + definition[1:]])
 
+            save_pickle(cph, MODELS_DIR + 'CoxPHM/' + str(x) + '_' + str(y) + '_' + str(T) + definition[1:])
