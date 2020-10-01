397939e5daf2e3479d186ffe54d335647ac5aa3d
Yue Wu <yuewu57@gmail.com>
2020-10-01 23:47:53 +0100

    Add files via upload

---
+++ src/models/LGBM/train_lgbm_cv.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import random
+import os
+import dill, pickle
+
+from lightgbm import LGBMClassifier
+
+
+from src.features.sepsis_mimic3_myfunction import *
+from src.features.LGBM.lgbm_functions import *
+
+
+if __name__ == '__main__':
+    
+    Root='/data/processed/LGBM/full_culture_data/'
+
+    a1,a2,k=6,0,5
+    x,y=24,12
+
+    Data_Dir=Root+str(x)+'_'+str(y)+'/cv/'
+    definitions=[ 't_sofa','t_suspicion', 't_sepsis_min']
+
+    print(Data_Dir)
+
+    results=[]
+
+    for definition in definitions:
+        
+            print(a1,definition)
+        
+            current_labels=np.load(Data_Dir+'label'+definition[1:]+'_'+str(a1)+'.npy')
+            feature_data=np.load(Data_Dir+'james_features'+definition[1:]+'.npy')
+            icustay_lengths=np.load(Data_Dir+'icustay_lengths'+definition[1:]+'.npy')
+           
+            tra_patient_indices,tra_full_indices,val_patient_indices,val_full_indices=
+                    cv_pack(icustay_lengths,k=k,definition=definition,path_save=Data_Dir,save=False)
+                
+            with open(Data_save+'lgbm_best_paras'+definition[1:]+'.pkl', 'rb') as file:
+                best_paras_=pickle.load(file)
+                
+
+
+            clf=LGBMClassifier(random_state=42).set_params(**best_paras_)
+            _, prob_preds, _,auc,specificity,accuracy=model_validation(clf,feature_data,\
+                                                                           current_labels,\
+                                                                           tra_full_indices,\
+                                                                           val_full_indices)
+
+            
+            np.save(Data_Dir+'prob_preds'+definition[1:]+'_'+str(a1)+'.npy',prob_preds)
+            
+            results.append([str(x)+','+str(y),a1,definition,auc,specificity,accuracy])
+        
+    result_df = pd.DataFrame(results, columns=['x,y','a1', 'definition', 'auc','speciticity','accuracy'])
+    result_df.to_csv(Data_Dir+"results.csv")
+
+ 
