38cdf7187f631456bd8f8d2c42e9c45c5ec01151
Yue Wu <yuewu57@gmail.com>
2020-10-01 23:31:22 +0100

    Update lgbm_tune.py

--- src/models/LGBM/lgbm_tune.py
+++ src/models/LGBM/lgbm_tune.py
@@ -10,55 +10,47 @@ from lightgbm import LGBMClassifier
 from src.features.sepsis_mimic3_myfunction import *
 from src.features.LGBM.lgbm_functions import *
 
-Root='/data/processed/LGBM/full_culture_data/'
-
-Data_save=Root+'results/'
-create_folder(Data_save)
-
-a1,a2,k=6,0,5
-x,y=24,12
-
-Data_Dir=Root+str(x)+'_'+str(y)+'/cv/'
 
 
-definitions=[ 't_sofa','t_suspicion', 't_sepsis_min']
+if __name__ == '__main__':
 
-print(Data_Dir)
+        Root='/data/processed/LGBM/full_culture_data/'
 
-model=LGBMClassifier(random_state=42)
-df_path='/data/raw/training_data/metavision_sepsis_data_18_09_20_sensitivity_'+str(x)+'_'+str(y)+'.csv'
+        Data_save=Root+'results/'
+        create_folder(Data_save)
 
-'
-feature_generator(Data_Dir,df_path)
+        a1,a2,k=6,0,5
+        x,y=24,12
+
+        Data_Dir=Root+str(x)+'_'+str(y)+'/cv/'
 
 
-for definition in definitions:
+        definitions=[ 't_sofa','t_suspicion', 't_sepsis_min']
+
+        print(Data_Dir)
+
+        model=LGBMClassifier(random_state=42)
+        df_path='/data/raw/training_data/metavision_sepsis_data_18_09_20_sensitivity_'+str(x)+'_'+str(y)+'.csv'
+
+
+        feature_generator(Data_Dir,df_path)
+
+
+        for definition in definitions:
         
-        current_labels=np.load(Data_Dir+'label'+definition[1:]+'_'+str(a1)+'.npy')
-        feature_data=np.load(Data_Dir+'james_features'+definition[1:]+'.npy')
-        icustay_lengths=np.load(Data_Dir+'icustay_lengths'+definition[1:]+'.npy')
+                current_labels=np.load(Data_Dir+'label'+definition[1:]+'_'+str(a1)+'.npy')
+                feature_data=np.load(Data_Dir+'james_features'+definition[1:]+'.npy')
+                icustay_lengths=np.load(Data_Dir+'icustay_lengths'+definition[1:]+'.npy')
            
-        tra_patient_indices,tra_full_indices,val_patient_indices,val_full_indices=
+                tra_patient_indices,tra_full_indices,val_patient_indices,val_full_indices=
                     cv_pack(icustay_lengths,k=k,definition=definition,path_save=Data_Dir,save=True)
         
 
-        lgbm_best_paras_=model_tuning(model,feature_data, current_labels,tra_full_indices,\
+                lgbm_best_paras_=model_tuning(model,feature_data, current_labels,tra_full_indices,\
                                       val_full_indices,grid_parameters, n_iter=1000)
         
 
-        with open(Data_save+'lgbm_best_paras'+definition[1:]+'.pkl', 'wb') as file:
-                pickle.dump(lgbm_best_paras_, file)
+                with open(Data_save+'lgbm_best_paras'+definition[1:]+'.pkl', 'wb') as file:
+                        pickle.dump(lgbm_best_paras_, file)
                 
-
-
-
-        
-        
-
-
-
-
-
-
-
 
