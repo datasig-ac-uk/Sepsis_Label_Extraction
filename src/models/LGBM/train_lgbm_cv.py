df1437e445ea7e6c319aebe9290ab247ff174ba9
Yue Wu <yuewu57@gmail.com>
2020-10-01 23:54:32 +0100

    Update train_lgbm_cv.py

--- src/models/LGBM/train_lgbm_cv.py
+++ src/models/LGBM/train_lgbm_cv.py
@@ -15,17 +15,29 @@ if __name__ == '__main__':
     
     Root='/data/processed/LGBM/full_culture_data/'
 
-    a1,a2,k=6,0,5
+    a2,k=0,5
     x,y=24,12
-
+    
     Data_Dir=Root+str(x)+'_'+str(y)+'/cv/'
-    definitions=[ 't_sofa','t_suspicion', 't_sepsis_min']
-
     print(Data_Dir)
+   
+    Data_save=Root+'results/'
+    
+    definitions=[ 't_sofa','t_suspicion', 't_sepsis_min']
+    T_list=[12,8,6,4]
+    
+     
+    df_path='/data/raw/training_data/metavision_sepsis_data_18_09_20_sensitivity_'+str(x)+'_'+str(y)+'.csv'
 
     results=[]
+    
+    for a1 in T_list:
+        
+        if a1!=6:
 
-    for definition in definitions:
+            feature_generator(Data_Dir,df_path,a1=a1)
+        
+        for definition in definitions:
         
             print(a1,definition)
         
@@ -53,6 +65,6 @@ if __name__ == '__main__':
             results.append([str(x)+','+str(y),a1,definition,auc,specificity,accuracy])
         
     result_df = pd.DataFrame(results, columns=['x,y','a1', 'definition', 'auc','speciticity','accuracy'])
-    result_df.to_csv(Data_Dir+"results.csv")
+    result_df.to_csv(Data_Dir+"cv_results.csv")
 
  
