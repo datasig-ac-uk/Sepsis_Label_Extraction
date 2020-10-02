a00ce9b5b649764b51cd7ca5370aeba266bebe42
Yue Wu <yuewu57@gmail.com>
2020-10-02 16:44:19 +0100

    Update train_lgbm_cv.py

--- src/models/LGBM/train_lgbm_cv.py
+++ src/models/LGBM/train_lgbm_cv.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     a2,k=0,5
     x,y=24,12
     
-    Data_Dir=Root+str(x)+'_'+str(y)+'/cv/'
+    Data_Dir=Root+'experiments_'+str(x)+'_'+str(y)+'/train/'
     print(Data_Dir)
    
     Data_save=Root+'results/'
@@ -46,8 +46,6 @@ if __name__ == '__main__':
                 
             with open(Data_save+'lgbm_best_paras'+definition[1:]+'.pkl', 'rb') as file:
                 best_paras_=pickle.load(file)
-                
-
 
             clf=LGBMClassifier(random_state=42).set_params(**best_paras_)
             _, prob_preds, _,auc,specificity,accuracy=model_validation(clf,feature_data,\
@@ -61,6 +59,6 @@ if __name__ == '__main__':
             results.append([str(x)+','+str(y),a1,definition,auc,specificity,accuracy])
         
     result_df = pd.DataFrame(results, columns=['x,y','a1', 'definition', 'auc','speciticity','accuracy'])
-    result_df.to_csv(Data_Dir+"lgbm_cv_results.csv")
+    result_df.to_csv(Data_save+"lgbm_cv_results.csv")
 
  
