367d614eb1814098741a74d71b75d43373e5c3e6
Hang Lou <lhang39@hotmail.com>
2020-10-23 10:36:20 +0100

    update

--- src/models/CoxPHM/eval_CoxPHM_signature.py
+++ src/models/CoxPHM/eval_CoxPHM_signature.py
@@ -9,39 +9,63 @@ from src.features.scaler import *
 from src.features.dicts import *
 from src.omni.functions import *
 from src.models.CoxPHM.coxphm_functions import *
-
+from src.features.sepsis_mimic3_myfunction import *
+from src.visualization.sepsis_mimic3_myfunction_patientlevel_clean import decompose_cms, output_at_metric_level
+from src.visualization.venn_diagram_plot import *
 if __name__ == '__main__':
-    x, y = 24, 12
-    
-    current_data_folder='full_culture_data/'    
-    Root_Data,Model_Dir,Data_save=folders(current_data_folder) 
-    
-    Data_Dir =Root_Data+'experiments_'+str(x)+'_'+str(y)+'/test/'
-    
     results = []
-    for definition in definitions:
+    results_patient_level=[]
+    train_test='/test'
+    T_list=[4,6,8,12]
+
+    for x, y in [(6,3),(12,6),(24, 12),(48,24)]:
+    
+        current_data_folder = 'blood_only_data/'
+        Root_Data,Model_Dir,Data_save=folders(current_data_folder,model='CoxPHM_no_sig')
+    
+        Data_Dir =Root_Data+'experiments_'+str(x)+'_'+str(y)+train_test+'/'
+    
+
+
         for T in T_list:
+            for definition in definitions:
+                print('load dataframe and input features')
+                df_sepsis = pd.read_pickle(Data_Dir + definition[1:] + '_dataframe.pkl')
+                features = np.load(Data_Dir + 'james_features' + definition[1:] + '.npy')
             
-            print('load dataframe and input features')
-            df_sepsis_test = pd.read_pickle(Data_Dir + definition[1:] + '_dataframe.pkl')
-            features_test = np.load(Data_Dir + 'james_features' + definition[1:] + '.npy')
-            
-            print('load test labels')
-            labels_test = np.load(Data_Dir + 'label' + definition[1:] + '_' + str(T) + '.npy')
+                print('load test labels')
+                labels = np.load(Data_Dir + 'label' + definition[1:] + '_' + str(T) + '.npy')
 
-            # prepare dataframe for coxph model
-            df_coxph_test = Coxph_df(df_sepsis_test, features_test, original_features, T, labels_test)
+                # prepare dataframe for coxph model
+                df_coxph = Coxph_df(df_sepsis, features, original_features, T, labels,signature=False)
+                #print(df_coxph.shape)
 
-            # load trained coxph model
-            cph = load_pickle(Model_Dir + str(x) + '_' + str(y) + '_' + str(T) + definition[1:])
+                # load trained coxph model
+                cph = load_pickle(Model_Dir + str(x) + '_' + str(y) + '_' + str(T) + definition[1:])
 
-            # predict and evalute on test set
-#             auc_score, specificity, accuracy = Coxph_eval(df_sepsis_test, cph, T,
-#                                                           OUTPUT_DIR + 'predictions/' + 'coxphm_' +
-#                                                           definition[1:] + '_' + str(T) + '.npy')
-            auc_score, specificity, accuracy = Coxph_eval(df_coxph_test, cph, T,None)
-            results.append([str(x) + ',' + str(y), T, definition, auc_score, specificity, accuracy])
+                #predict and evalute on test set
+                create_folder(OUTPUT_DIR + 'predictions/' +current_data_folder+ 'CoxPHM_no_sig/')
+                auc_score, specificity, accuracy = Coxph_eval(df_coxph, cph, T,
+                                                           OUTPUT_DIR + 'predictions/' +current_data_folder+ 'CoxPHM_no_sig/'
+                                                              +str(x) + '_' + str(y) + '_' + str(T)+
+                                                              definition[1:]  + '.npy')
+                preds = np.load(  OUTPUT_DIR + 'predictions/' +current_data_folder+ 'CoxPHM_no_sig/'
+                                                              +str(x) + '_' + str(y) + '_' + str(T)+
+                                                              definition[1:]  + '.npy')
+                #CMs, _,_=suboptimal_choice_patient(df_sepsis, labels, preds, a1=6, thresholds=np.arange(10000) / 10000,
+                 #                         sample_ids=None)
+                #tprs, tnrs, fnrs, pres, accs = decompose_cms(CMs)
 
-    # save numerical results
+                #results_patient_level.append([str(x) + ',' + str(y), T,definition, "{:.3f}".format(auc(1 - tnrs, tprs)), \
+                        #        "{:.3f}".format(output_at_metric_level(tnrs, tprs, metric_required=[0.85])), \
+                          #      "{:.3f}".format(output_at_metric_level(accs, tprs, metric_required=[0.85]))])
+
+                results.append([str(x) + ',' + str(y), T, definition, auc_score, specificity, accuracy])
+
+        # save numerical results
+    create_folder(OUTPUT_DIR + 'results/' + current_data_folder + 'CoxPHM_no_sig/')
+   # results_patient_level_df = pd.DataFrame(results_patient_level, columns=['x,y', 'T','definition', 'auc', 'sepcificity', 'accuracy'])
+
+   # results_patient_level_df.to_csv(OUTPUT_DIR + 'results/'+current_data_folder+ 'CoxPHM_no_sig'+train_test+'patient_level_results.csv')
     result_df = pd.DataFrame(results, columns=['x,y', 'T', 'definition', 'auc', 'speciticity', 'accuracy'])
-    result_df.to_csv(Data_save+str(x) + '_' + str(y)+'_coxph_test_results.csv')
+    result_df.to_csv(OUTPUT_DIR + 'results/'+current_data_folder+ 'CoxPHM_no_sig'+train_test+'results.csv')
--- src/models/CoxPHM/tune_CoxPHM.py
+++ src/models/CoxPHM/tune_CoxPHM.py
@@ -9,23 +9,12 @@ from src.models.CoxPHM.coxphm_functions import *
 import ray
 from ray import tune
 from ray.tune.utils import pin_in_object_store, get_pinned_object
-
-
-
-
-
-
-
-
-
-
-
-
-
+from src.features.sepsis_mimic3_myfunction import *
 
 if __name__ == '__main__':
     current_data = 'blood_only_data/'
-    Root_Data, Model_Dir, _ = folders(current_data)
+    signature=False
+    Root_Data, Model_Dir, _ = folders(current_data, model='CoxPHM_no_sig')
 
     a1, a2, k = 6, 0, 5
     x, y = 24, 12
@@ -34,7 +23,7 @@ if __name__ == '__main__':
 
     print(Data_Dir)
 
-    for definition in definitions:
+    for definition in ['t_sofa']:
         labels = np.load(Data_Dir + 'label' + definition[1:] + '_' + str(a1) + '.npy')
         df = pd.read_pickle(Data_Dir + definition[1:] + '_dataframe.pkl')
         features = np.load(Data_Dir + 'james_features' + definition[1:] + '.npy')
@@ -44,10 +33,10 @@ if __name__ == '__main__':
             cv_pack(icustay_lengths, k=k, definition=definition, path_save=Data_Dir, save=True)
 
         # prepare dataframe for coxph model
-        df_coxph = Coxph_df(df, features, original_features, a1, labels)
+        df_coxph = Coxph_df(df, features, original_features, a1, labels,signature=False)
         ray.init(num_cpus=5)
-        data = pin_in_object_store([df_coxph,  tra_full_indices, val_full_indices, k])
-        analysis = tune.run(partial(model_cv,data_list=data,a1=a1),
+        data = pin_in_object_store([df_coxph, tra_full_indices, val_full_indices, k])
+        analysis = tune.run(partial(model_cv, data=data, a1=a1),
                             name='mimic_coxph' + definition[1:], config=search_space,
                             resources_per_trial={"cpu": 1}, num_samples=100,
                             max_failures=5, reuse_actors=True, verbose=1)
@@ -55,4 +44,5 @@ if __name__ == '__main__':
         print("Best trial config: {}".format(best_trial.config))
         print("Best trial final validation auc: {}".format(
             best_trial.last_result["mean_accuracy"]))
-        save_pickle(best_trial.config, Data_Dir +'hyperparameter/' + 'config' + definition[1:])
+        save_pickle(best_trial.config, Model_Dir + 'hyperparameter/' + 'config' + definition[1:])
+        ray.shutdown()
