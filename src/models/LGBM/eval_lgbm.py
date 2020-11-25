fcc50e41296f7f17c520a28a66696fae67d6ea86
Yue Wu <wuy@maths.ox.ac.uk>
2020-11-25 17:01:04 +0000

    changes

--- src/models/LGBM/eval_lgbm.py
+++ src/models/LGBM/eval_lgbm.py
@@ -21,13 +21,14 @@ if __name__ == '__main__':
     x,y=24,12
 
     current_data='blood_culture_data/'
-    Root_Data,Model_Dir,Data_save=folders(current_data,model=MODELS[0])
+    Root_Data,Model_Dir,_,Output_predictions,Output_results=folders(current_data,model=MODELS[0])
     
+    results=[]
     for x,y in xy_pairs:
         Data_Dir_train=Root_Data+'experiments_'+str(x)+'_'+str(y)+'/train/'
         Data_Dir_test=Root_Data+'experiments_'+str(x)+'_'+str(y)+'/test/'
         
-        results=[]
+        
     
         for a1 in T_list:
             
@@ -41,19 +42,20 @@ if __name__ == '__main__':
                 label_test=np.load(Data_Dir_test+'label'+definition[1:]+'_'+str(a1)+'.npy')
                 feature_test=np.load(Data_Dir_test+'james_features'+definition[1:]+'.npy')
         
-                with open(Data_save+'lgbm_best_paras'+definition[1:]+'.pkl', 'rb') as file:
+                with open(Model_Dir+'lgbm_best_paras'+definition[1:]+'.pkl', 'rb') as file:
                     best_paras_=pickle.load(file)
                 
                 clf=LGBMClassifier(random_state=42).set_params(**best_paras_)
             
                 _, prob_preds_test, auc,specificity,accuracy=model_training(clf,feature_train,feature_test,label_train,label_test)
         
-                np.save(Data_Dir_test+'lgbm_prob_preds'+definition[1:]+'_'+str(a1)+'.npy',prob_preds_test)
+                np.save(Output_predictions+'prob_preds_'+str(x)+'_'+str(y)+'_'+str(a1)+'_'+definition[1:]+'.npy',prob_preds_test)
                 results.append([str(x)+','+str(y),a1,definition,auc,specificity,accuracy])
         
     result_df = pd.DataFrame(results, columns=['x,y','a1', 'definition', 'auc','speciticity','accuracy'])
-    result_df.to_csv(Data_save+str(x)+ '_' +str(y)+'_lgbm_test_results.csv')
-    main_result_tables(result_df,Data_save,model='lgbm',purpose='test')
+    result_df.to_csv(Output_predictions+'lgbm_test_results.csv')
+    
+    main_result_tables(result_df,Output_results,model='lgbm',purpose='test')
 
         
 
