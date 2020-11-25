9b4d64255f993a3e745c00cab41e528ecbcae924
Yue Wu <wuy@maths.ox.ac.uk>
2020-11-25 16:44:32 +0000

    changes

--- src/models/LGBM/train_lgbm_cv.py
+++ src/models/LGBM/train_lgbm_cv.py
@@ -17,15 +17,17 @@ from src.models.LGBM.lgbm_functions import *
 if __name__ == '__main__':
     
     current_data='blood_culture_data/'
-    Root_Data,Model_Dir,Data_save=folders(current_data,model=MODELS[0])
-
+    Root_Data,Model_Dir,Data_save,Output_predictions,Output_results=folders(current_data,model=MODELS[0])
+    
+    Output_predictions_cv=Output_predictions+'cv/'
+    create_folder(Output_predictions_cv)
     a2,k=0,5
 
 
     results=[]
     
     for x,y in xy_pairs:
-        Data_Dir=Root_Data+'experiments_'+str(x)+'_'+str(y)+'/train/'
+        Data_Dir=Root_Data+'experiments_'+str(x)+'_'+str(y)+'/cv/'
         print(Data_Dir)
 
         for a1 in T_list:    
@@ -39,12 +41,12 @@ if __name__ == '__main__':
                                                                                      a1)
                 
                  
-                np.save(Data_Dir+'prob_preds'+definition[1:]+'_'+str(a1)+'.npy',prob_preds)
+                np.save(Output_predictions_cv+'prob_preds_'+str(x)+'_'+str(y)+'_'+str(a1)+definition[1:]+'.npy',prob_preds)
             
                 results.append([str(x)+','+str(y),a1,definition,auc,specificity,accuracy])
         
     result_df = pd.DataFrame(results, columns=['x,y','a1','definition', 'auc','speciticity','accuracy'])
-    result_df.to_csv(Data_save+"lgbm_cv_results.csv")
-#     result_df=pd.read_csv(Data_save+"lgbm_cv_results.csv")
+    result_df.to_csv(Output_results+"lgbm_cv_results.csv")
+
     main_result_tables(result_df,Data_save,model='lgbm',purpose='cv')
  
