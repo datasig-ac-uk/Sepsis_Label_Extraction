d9d4e1de4aa919ff59741d8b1e34acc7e0116e5f
HangL-39 <hang.lou.19@ucl.ac.uk>
2020-12-22 17:47:40 +0800

    update visualization

--- src/visualization/main_plots.py
+++ src/visualization/main_plots.py
@@ -5,112 +5,135 @@ import random
 import os
 import pickle
 
+import os
+import pickle
+import random
 import sys
-sys.path.insert(0, '../../')
 
-from definitions import *
+import numpy as np
+import pandas as pd
 
-from src.features.sepsis_mimic3_myfunction import *
-from src.visualization.sepsis_mimic3_myfunction_patientlevel_clean import *
-from src.visualization.plot_functions_clean import *
-from src.visualization.table_functions_clean import *
+sys.path.insert(0, '../')
+
+import constants
+
+import features.sepsis_mimic3_myfunction as mimic3_myfunc
+import visualization.sepsis_mimic3_myfunction_patientlevel_clean as patientlevel_clean
+import visualization.plot_functions_clean as plot_functions_clean
+import visualization.table_functions_clean as table_functions_clean
 
 if __name__ == '__main__':
 
+    print("Collecting results at setting x,y,T=24,12,6 from three models",
+          constants.MODELS[0], constants.MODELS[1], constants.MODELS[-1])
 
-    print("Collecting results at setting x,y,T=24,12,6 from three models", MODELS[0],MODELS[1],MODELS[-1])
-    
-    Root_Data=DATA_processed+'blood_culture_data/'
-    Data_Dir=Root_Data+'experiments_24_12/test/'
+    current_data = 'blood_culture_data/'
+    Root_Data = constants.DATA_processed + current_data
+
+    Data_Dir = Root_Data + 'experiments_24_12/test/'
     print("The interim results will be collected from ", Data_Dir)
-    
-    Data_save_plots=Root_Data+'plots/'
-    create_folder(Data_save_plots)
-    
-    Data_save_tables=Root_Data+'tables/' 
-    create_folder(Data_save_tables)
-    
-    labels_list_list=[]
-    probs_list_list=[]
-    test_indices_list_list=[]
-    icuid_sequence_list_list=[]
-    n=100
-    for model in models:
-        
-        print("Now Collecting results from model", model)
-        
-        labels_list=[]
-        probs_list=[]
-        test_indices_list=[]
 
-        
-        for definition in definitions:
-        
-            labels_now=np.load(Data_Dir+'label'+definition[1:]+'_6.npy')    
-            probs_now=np.load(Data_Dir+model+'_prob_preds'+definition[1:]+'_6.npy')
-        
-            icu_lengths_now=np.load(Data_Dir+'icustay_lengths'+definition[1:]+'.npy')        
-            icustay_fullindices_now=patient_idx(icu_lengths_now)
-                        
+    Data_save_plots = Root_Data + 'plots/'
+    mimic3_myfunc.create_folder(Data_save_plots)
+
+    Data_save_tables = Root_Data + 'tables/'
+    mimic3_myfunc.create_folder(Data_save_tables)
+
+    labels_list_list = []
+    probs_list_list = []
+    test_indices_list_list = []
+    icuid_sequence_list_list = []
+
+    x, y, a1 = 24, 12, 6
+    n, n_bootstraps = 100, 100
+
+    mean_fpr_list = [np.linspace(0, 1, 30) for i in range(3)]  ### for auc with CI plots
+
+    for model in constants.models:
+
+        print("Now Collecting results from model", model)
+
+        labels_list = []
+        probs_list = []
+        test_indices_list = []
+        _, _, _, Output_predictions, _ = mimic3_myfunc.folders(current_data, model=model)
+
+        for definition in constants.FEATURES:
+            labels_now = np.load(Data_Dir + 'label' + definition[1:] + '_6.npy')
+            probs_now = np.load(
+                Output_predictions + 'prob_preds_' + str(x) + '_' + str(y) + '_' + str(a1) + '_' + definition[
+                                                                                                   1:] + '.npy')
+
+            icu_lengths_now = np.load(Data_Dir + 'icustay_lengths' + definition[1:] + '.npy')
+            icustay_fullindices_now = patientlevel_clean.patient_idx(icu_lengths_now)
+
             labels_list.append(labels_now)
             probs_list.append(probs_now)
             test_indices_list.append(icustay_fullindices_now)
 
-
         labels_list_list.append(labels_list)
         probs_list_list.append(probs_list)
         test_indices_list_list.append(test_indices_list)
-    
+
     ######### Instance level auc plot/table #################
     print("Instance level now:")
-    
-    names=['H1','H2','H3']
+
+    names = ['H1', 'H2', 'H3']
     print("Instance level AUC plots for three models.")
-    auc_subplots(labels_list_list,probs_list_list,names,save_name=Data_save_plots+"auc_plot_instance_level_three_models_test")
-    
+    print('Now 95% CI:')
+    fprs_lists, tprs_lists = plot_functions_clean.fprs_tprs_output(labels_list_list, probs_list_list,
+                                                                   n_bootstraps=n_bootstraps)
+
+    error_list = plot_functions_clean.CI_std_output(fprs_lists, tprs_lists, mean_fpr_list=mean_fpr_list)
+
+    print("Plotting instance-level aucroc with CI for three models.")
+    plot_functions_clean.auc_subplots(labels_list_list, probs_list_list, error_list, names, mean_fpr_list=mean_fpr_list, \
+                                      save_name=Data_save_plots + 'auc_plot_instance_level_three_models_test')
+
     print("Saving instance-level auc scores for three models.")
-    instance_level_auc_pd_threemodels(labels_list_list,probs_list_list,\
-                                      pd_save_name=Data_save_tables+"auc_instance_level_three_models_test")
-    
+    table_functions_clean.instance_level_auc_pd_threemodels(labels_list_list, probs_list_list, \
+                                                            pd_save_name=Data_save_tables + "auc_instance_level_three_models_test")
+
     ######### Patient level auc plots/tables #################
     print("Patient level now:")
-    tprs_list_list,fprs_list_list, fnrs_list_list,pres_list_list,\
-    accs_list_list,time_list_list=patient_level_main_outputs_threemodels(labels_list_list,\
-                                                                           probs_list_list,\
-                                                                           test_indices_list_list,\
-                                                                           n=n,\
-                                                                           icuid_sequence_list_list=None)
-    print("Patient level AUC plots for three models.")
-    auc_subplots_patient_level(fprs_list_list,tprs_list_list,names,titles=MODELS,\
-                               save_name=Data_save_plots+"auc_plot_patient_level_three_models_test")
-    
+
+    fprs_lists_par, tprs_lists_par, labels_list_par, probs_list_par = plot_functions_clean.fprs_tprs_output_patient_level(
+        labels_list_list, \
+        probs_list_list, \
+        test_indices_list_list)
+    print("Plotting patient-level aucroc with CI for three models.")
+    error_list_par = plot_functions_clean.CI_std_output(fprs_lists_par, tprs_lists_par, mean_fpr_list=mean_fpr_list)
+
+    plot_functions_clean.auc_subplots(labels_list_par, probs_list_par, error_list_par, names,
+                                      mean_fpr_list=mean_fpr_list, \
+                                      save_name=Data_save_plots + 'auc_plot_patient_level_three_models_test')
+
+    print("A different way of getting tprs/fprs for three models.")
+    tprs_list_list, fprs_list_list, fnrs_list_list, pres_list_list, \
+    accs_list_list, time_list_list = patientlevel_clean.patient_level_main_outputs_threemodels(labels_list_list, \
+                                                                                               probs_list_list, \
+                                                                                               test_indices_list_list, \
+                                                                                               n=n, \
+                                                                                               icuid_sequence_list_list=None)
+
     print("Patient level recall-precision plots for three models.")
-    recall_specificity_subplots_patient_level(pres_list_list,tprs_list_list,names,\
-                               save_name=Data_save_plots+"recall_precision_plot_patient_level_three_models_test")
-    
+    plot_functions_clean.recall_specificity_subplots_patient_level(pres_list_list, tprs_list_list, names, \
+                                                                   save_name=Data_save_plots + "recall_precision_plot_patient_level_three_models_test")
+
     print("Saving patient-level auc scores for three models.")
-    patient_level_auc_pd_threemodels(fprs_list_list,tprs_list_list,for_write=False,\
-                                     pd_save_name=Data_save_tables+"auc_patient_level_three_models_test")
-    
+    table_functions_clean.patient_level_auc_pd_threemodels(fprs_list_list, tprs_list_list, for_write=False, \
+                                                           pd_save_name=Data_save_tables + "auc_patient_level_three_models_test")
+
     print("Saving patient-level specificity at fixed sensitivity level 0.85 for three models.")
-    patient_level_output_pd_threemodels(fprs_list_list,tprs_list_list,metric_required=[0.85],\
-                                        operator=lambda x: 1-x, for_write=False,\
-                                     pd_save_name=Data_save_tables+"specificity_patient_level_three_models_test")
-    
+    table_functions_clean.patient_level_output_pd_threemodels(fprs_list_list, tprs_list_list, metric_required=[0.85], \
+                                                              operator=lambda x: 1 - x, for_write=False, \
+                                                              pd_save_name=Data_save_tables + "specificity_patient_level_three_models_test")
+
     print("Saving patient-level accuracy at fixed sensitivity level 0.85 for three models.")
-    patient_level_output_pd_threemodels(accs_list_list,tprs_list_list,metric_required=[0.85],\
-                                        operator=lambda x: x, for_write=False,\
-                                        pd_save_name=Data_save_tables+"accuracy_patient_level_three_models_test")
-    ######### Patient level early prediction performance #################    
-    
-    print("Box plot (test set) of how far in advance sepsis casesbeing predicted correctly at sensitivity 85%")
-    time_prediction_in_advance_list_list_tpr=patient_level_threded_output_threemodels(time_list_list,\
-                                                                                  tprs_list_list,\
-                                                                                  metric_required=[0.85])
-    ylabel='Hour'
-    name_seqs=names
-    boxplots_prediction_time_inadvance(time_prediction_in_advance_list_list_tpr,\
-                                       name_seqs, ylabel,\
-                                       savetitle=Data_save_plots+'boxplot_three_models_tpr_test')
-    
-    
+    table_functions_clean.patient_level_output_pd_threemodels(accs_list_list, tprs_list_list, metric_required=[0.85], \
+                                                              operator=lambda x: x, for_write=False, \
+                                                              pd_save_name=Data_save_tables + "accuracy_patient_level_three_models_test")
+
+
+
+
