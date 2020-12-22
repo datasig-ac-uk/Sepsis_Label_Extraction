625b767baded4c02b6e70e5ef8bdf940435f21c0
Yue Wu <yuewu57@gmail.com>
2020-12-22 12:01:33 +0000

    Update generate_test_features.py

--- src/features/generate_test_features.py
+++ src/features/generate_test_features.py
@@ -5,14 +5,6 @@ import constants
 import features.sepsis_mimic3_myfunction as sepsis_mimic3
 
 if __name__ == '__main__':
-
-import sys
-
-sys.path.insert(0, '../')
-import constants
-import features.sepsis_mimic3_myfunction as sepsis_mimic3
-
-if __name__ == '__main__':
     
     for x,y in xy_pairs:
     
