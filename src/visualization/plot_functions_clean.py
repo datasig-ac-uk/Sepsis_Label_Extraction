0dc7c927ecf752c8b1168f154d08b95cda57a390
Yue Wu <yuewu57@gmail.com>
2021-01-07 10:47:25 +0000

    Update plot_functions_clean.py

--- src/visualization/plot_functions_clean.py
+++ src/visualization/plot_functions_clean.py
@@ -330,6 +330,148 @@ def fprs_tprs_output_patient_level(labels_list_list, probs_list_list, indices_li
 
     return fprs_lists, tprs_lists, labels_list, probs_list
 
+colors_shade=[sns.color_palette("Pastel2")[0],sns.color_palette("Pastel2")[2],sns.color_palette("Pastel2")[1]]
+colors_auc=sns.color_palette("Dark2")
+colors_auc=[colors_auc[0],colors_auc[2],colors_auc[1]]
+
+def auc_subplots_errorbars(trues_list, probs_list,error_lists,names,\
+                           mean_fpr_list=[np.linspace(0, 1, 30+0*i) for i in range(3)],\
+                           fontsize=14,figsize=(15,5), titles=MODELS,\
+                           colors=colors_shade,colors_line=colors_auc,linestyles=linestyles,lw=2,\
+                           loc="lower right",save_name=None):
+    
+    """
+        
+        AUC plots for different models via ground truth and predicted probabilities
+        
+    Input:
+    
+        trues_list: list of ground-truth-seq lists 
+        
+                eg, for three models for 2 set of data,[[model1 truth-list], [model2 truth-list], [model3 truth-list]]
+                    [model1 truth-list]=[[ground truth for set1],[ground truth for set2]]
+                
+        probs_list: probability-seq list
+        
+            eg, for three models for 2 set of data,  [[model1 probs-list], [model2 probs-list], [model3 probs-list]]
+            
+                [model1 probs-list]=[[probabilities for set1],[probabilities for set2]]
+            
+        names: curve labels for sets of data
+        
+        save_name: if None: print figure; else: save to save_name.png
+
+        
+    
+    """
+
+    plt.figure(figsize=figsize)
+    plt.subplot(131)
+    
+    num=len(trues_list[0])
+    
+    for i in range(num):
+        
+        fpr, tpr, _ = roc_curve(trues_list[0][i], probs_list[0][i])
+        roc_auc = auc(fpr, tpr)
+        
+        mean_tpr=np.interp(mean_fpr_list[i],fpr,tpr)
+        plt.plot(mean_fpr_list[i], mean_tpr,  color=colors_line[i],\
+                    lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         plt.errorbar(mean_fpr_list[i], mean_tpr, error_lists[-1][i], color=colors[i],\
+#                     lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        plt.fill_between(mean_fpr_list[i], mean_tpr-error_lists[-1][i], mean_tpr+error_lists[-1][i], color=colors[i])
+
+#         plt.plot(fpr, tpr,  color=colors_line[i], lw=lw,linestyle=linestyles[i],\
+#                  label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         error=np.interp(fpr,mean_fpr_list[i], error_lists[0][i])
+#         plt.fill_between(fpr, tpr-error, tpr+error, color=colors[i])
+        
+    plt.plot([0, 1], [0, 1], color='gray', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate',fontsize=fontsize)
+    plt.ylabel('True Positive Rate',fontsize=fontsize)
+    plt.title(titles[0])
+    plt.legend(loc=loc,fontsize=fontsize-3)
+    plt.xticks(fontsize=fontsize-3)
+    plt.yticks(fontsize=fontsize-3)
+    
+    plt.subplot(132)
+    
+    num=len(trues_list[1])
+    for i in range(num):
+        
+        fpr, tpr, _ = roc_curve(trues_list[1][i], probs_list[1][i])
+        roc_auc = auc(fpr, tpr)
+        
+
+        mean_tpr=np.interp(mean_fpr_list[i],fpr,tpr)
+        
+        plt.plot(mean_fpr_list[i], mean_tpr,  color=colors_line[i],\
+                    lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         plt.errorbar(mean_fpr_list[i], mean_tpr, error_lists[-1][i], color=colors[i],\
+#                     lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        plt.fill_between(mean_fpr_list[i], mean_tpr-error_lists[-1][i], mean_tpr+error_lists[-1][i], color=colors[i])
+
+#         plt.plot(fpr, tpr,  color=colors_line[i], lw=lw,linestyle=linestyles[i],\
+#                  label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         error=np.interp(fpr,mean_fpr_list[i], error_lists[1][i])
+#         plt.fill_between(fpr, tpr-error, tpr+error, color=colors[i])
+        
+ 
+    plt.plot([0, 1], [0, 1], color='gray', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate',fontsize=fontsize)
+    plt.title(titles[1])
+    plt.legend(loc=loc,fontsize=fontsize-3)
+    plt.xticks(fontsize=fontsize-3)
+    plt.yticks(fontsize=fontsize-3)
+    
+    
+    plt.subplot(133)
+    
+    num=len(trues_list[-1])
+    for i in range(num):
+        
+        fpr, tpr, _ = roc_curve(trues_list[-1][i], probs_list[-1][i])
+        roc_auc = auc(fpr, tpr)
+        
+        mean_tpr=np.interp(mean_fpr_list[i],fpr,tpr)
+        plt.plot(mean_fpr_list[i], mean_tpr,  color=colors_line[i],\
+#                     lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+                    lw=lw,linestyle=linestyles[i],label=names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         plt.errorbar(mean_fpr_list[i], mean_tpr, error_lists[-1][i], color=colors[i],\
+#                     lw=lw,linestyle=linestyles[i],label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        plt.fill_between(mean_fpr_list[i], mean_tpr-error_lists[-1][i], mean_tpr+error_lists[-1][i], color=colors[i])
+
+#         plt.plot(fpr, tpr,  color=colors_line[i], lw=lw,linestyle=linestyles[i],\
+#                  label='ROC curve for '+names[i] +' (area = %0.2f)' % roc_auc)
+        
+#         error=np.interp(fpr,mean_fpr_list[i], error_lists[-1][i])
+#         plt.fill_between(fpr, tpr-error, tpr+error, color=colors[i])
+
+    plt.plot([0, 1], [0, 1], color='gray', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate',fontsize=fontsize)
+    plt.title(titles[-1])
+    plt.legend(loc=loc,fontsize=fontsize-3)
+    plt.xticks(fontsize=fontsize-3)
+    plt.yticks(fontsize=fontsize-3)
+    
+    if save_name is not None:
+        plt.savefig(save_name+'.jpeg',dpi=350)
+    else:
+        
+        plt.show()
+
 
 def recall_specificity_subplots_patient_level(pres_list, tprs_list, names, \
                                               fontsize=14, figsize=(15, 5), \
