71939caa63217064f6f14ab6b0b7b02a9b4421ab
Yue Wu <yuewu57@gmail.com>
2021-01-28 22:17:29 +0000

    Update generate_test_features.py

--- src/features/generate_test_features.py
+++ src/features/generate_test_features.py
@@ -2,21 +2,33 @@ import sys
 
 sys.path.insert(0, '../')
 import constants
-import features.sepsis_mimic3_myfunction as sepsis_mimic3
+import features.sepsis_mimic3_myfunction as mimic3_myfunc
 
 if __name__ == '__main__':
+   
+    
+    current_data='blood_culture_data/'
+ 
+    purpose='test' ### Otherwise 'train'
+        
+    Root_Data, _, _, _,_ = mimic3_myfunc.folders(current_data)
+    
+    Save_Dir =  Root_Data+purpose+'/'
     
     for x,y in xy_pairs:
     
       a2 = 0
-
-      path_df_test = '/scratch/mimiciii/training_data/further_split/val_{}_{}.csv'.format(x, y)
-
-      Save_Dir_test =  constants.DATA_processed + 'full_culture_data/experiments_{}_{}/test/'.format(x, y)
-
-      print('generate train/set features for sensitity {}_{} definition'.format(x, y))
-
-      sepsis_mimic3.featureset_generator(path_df_test, Save_Dir_test, x=x, y=y, a2=a2, definitions=constants.FEATURES, T_list=constants.T_list)
+        
+      if purpose=='test':
+        path_df = '/scratch/mimiciii/training_data/further_split/val_{}_{}.csv'.format(x, y)        
+      elif purpose=='train':
+        path_df = '/scratch/mimiciii/training_data/further_split/train_{}_{}.csv'.format(x, y)    
+      else:
+        raise TypeError("purpose not recognised!")
+ 
+      print('generate '+purpose+' features for sensitity {}_{} definition'.format(x, y))
+    
+      mimic3_myfunc.featureset_generator(path_df, Save_Dir, x=x, y=y, a2=a2, definitions=constants.FEATURES, T_list=constants.T_list)
 
 
 """
