3acee9e3f691570de95211a0b4640391e1d2b6a0
HangL-39 <hang.lou.19@ucl.ac.uk>
2021-03-02 12:16:45 +0000

    make model main file

--- src/models/main.py
+++ src/models/main.py
@@ -1,2 +1,7 @@
-from src.models.LSTM.train_lstm import *
-from src.models.CoxPHM.train_CoxPHM_signature import *
+import os
+import argparse
+model_parser = argparse.ArgumentParser(description='Argument for specifying model types and process')
+model_parser.add_argument('--model',type = str,required=True,default='lgbm')
+model_parser.add_argument('--process',type = str,required=True,default='train')
+args = model_parser.parse_args()
+os.system('python3 '+'models/'+args.model+'/'+args.process+'_'+args.model+'.py')
