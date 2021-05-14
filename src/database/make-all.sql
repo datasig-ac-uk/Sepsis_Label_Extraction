4b6b37b5fc25cdb067771c9501f7e4efd6126ab6
Peter Foster <pafoster@users.noreply.github.com>
2021-05-14 20:54:01 +0100

    Data extraction pipeline - merge fixes (#6)
    * add times
    
    * add mapping keys
    
    * Include syntax
    
    * abx table
    
    * Add requirements file for split generation
    
    Co-authored-by: Lingyi Yang <yangl@maths.ox.ac.uk>

--- src/database/make-all.sql
+++ src/database/make-all.sql
@@ -1,15 +1,19 @@
 /* This query loads the necessary tables to prepare for the sepsis definition */
 
-.read vitals.sql
-.read labs.sql
-.read current_icu.sql
+\ir vitals.sql
+\ir labs.sql
+\ir current_icu.sql
 
-.read pivoted-bg-mod.sql
-.read pivoted-lab-ffill.sql
-.read pivoted-sofa-mod.sql
+\ir times.sql
 
-.read extracted_data.sql
+\ir pivoted-bg-mod.sql
+\ir pivoted-lab-ffill.sql
+\ir pivoted-sofa-mod.sql
 
-.read sepsis-time-all.sql
-.read sepsis-time-abs.sql
-.read sepsis-time-nogcs.sql
+\ir extracted_data.sql
+
+\ir abx-poe-list.sql
+\ir sepsis-time-all.sql
+\ir sepsis-time-abs.sql
+\ir sepsis-time-nogcs.sql
+
