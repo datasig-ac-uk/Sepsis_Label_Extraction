93bcc8838bc263faee3e5838196152c424759e20
Peter Foster <pafoster@users.noreply.github.com>
2021-05-28 12:24:24 +0100

    Lingyi (#11)
    * add times
    
    * add mapping keys
    
    * Include syntax
    
    * abx table
    
    * Add requirements file for split generation
    
    * changed cultures and notebook
    
    * structure
    
    * fix table ref
    
    * fix join issues
    
    * fix typo
    
    * updated train notebook and file names
    
    Co-authored-by: Lingyi Yang <yangl@maths.ox.ac.uk>

--- src/database/make-all.sql
+++ src/database/make-all.sql
@@ -1,8 +1,8 @@
 /* This query loads the necessary tables to prepare for the sepsis definition */
 
-\ir vitals.sql
+\ir extracted-chart.sql
 \ir labs.sql
-\ir current_icu.sql
+\ir current-icu.sql
 
 \ir times.sql
 \ir assign-lab-ids.sql
@@ -11,13 +11,13 @@
 \ir pivoted-lab-ffill.sql
 \ir pivoted-sofa-mod.sql
 
-\ir extracted_data.sql
+\ir extracted-data.sql
 
 \ir abx-poe-list.sql
 
-\ir culture_list.sql
+\ir culture-list.sql
 
 \ir sepsis-time-cultures.sql
-\ir sepsis-time-abs.sql
-\ir sepsis-time-nogcs.sql
+--\ir sepsis-time-abs.sql
+--\ir sepsis-time-nogcs.sql
 
