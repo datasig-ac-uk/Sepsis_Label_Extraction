1a81541fbd767a19e39b21298ebee0d9ae452cc0
Peter Foster <pafoster@users.noreply.github.com>
2021-06-03 01:14:48 +0100

    Lingyi (#12)
    * add times
    
    * add mapping keys
    
    * Include syntax
    
    * abx table
    
    * Add requirements file for split generation
    
    * changed cultures and notebook
    
    * structure
    
    * fix table ref
    
    * fix join issues
    
    * fix typo
    
    * updated train notebook and file names
    
    * change id path
    
    Co-authored-by: Lingyi Yang <yangl@maths.ox.ac.uk>

--- src/database/id_tables.sql
+++ src/database/id_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE train_ids(subject_id integer);
-\COPY train_ids FROM './split_ids/train_id.csv' CSV DELIMITER ',';
+\COPY train_ids FROM './original_ids/train_id.csv' CSV DELIMITER ',';
 
 CREATE TABLE val_ids(subject_id integer);
-\COPY val_ids FROM './split_ids/test_id1.csv' CSV DELIMITER ',';
+\COPY val_ids FROM './original_ids/test_id1.csv' CSV DELIMITER ',';
 
 CREATE TABLE test_ids(subject_id integer);
-\COPY test_ids FROM './split_ids/test_id2.csv' CSV DELIMITER ',';
+\COPY test_ids FROM './original_ids/test_id2.csv' CSV DELIMITER ',';
 
