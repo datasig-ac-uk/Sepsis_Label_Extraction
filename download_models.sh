a07b5973b6240281b85e28fa978ef5e630ab0a27
HangL-39 <hang.lou.19@ucl.ac.uk>
2021-08-09 14:36:54 +0100

    update requirements.txt

--- download_models.sh
+++
@@ -1,7 +0,0 @@
-#pip install gdown
-
-gdown --id 17FZNA65asNMVxT9YCzqv0Epur-Dnj3KJ
-unzip models.zip
-rm -rf model.zip
-
-https://drive.google.com/file/d/17FZNA65asNMVxT9YCzqv0Epur-Dnj3KJ/view?usp=sharing
