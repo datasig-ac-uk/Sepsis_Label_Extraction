3a2995d76d5e45aa8656c038e0fa262473c037ff
Peter Foster <pfoster@turing.ac.uk>
2021-08-18 15:43:59 +0100

    Define n_cpus in constants module for LGBM functions

--- src/constants.py
+++ src/constants.py
@@ -28,3 +28,6 @@ MIMIC_DATA_DIRS['blood_only'] = MIMIC_DATA_DIRS['strict_exclusion']
 
 MIMIC_DATA_DIRS['other_cultures'] = {'train': DATA_DIR+'raw/train/other_cultures',
                                      'test': DATA_DIR + 'raw/test/other_cultures'}
+
+N_CPUS = 1
+N_GPUS = 1
